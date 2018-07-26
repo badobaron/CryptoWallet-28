bfdebe39c8933bc8947023b007d58eeee95a601c
Kiana Harrell <kianaharrell@gmail.com>
2018-07-26 17:04:47 -0400

    Page layout added

---
+++ pages/dashboard.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default () => {
+    return <h1>This will be the user dashboard page when logged in.</h1>;
+};
---
+++ pages/new.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default () => {
+    return <h1>This will be the page to initiate new transactions.</h1>;
+};
